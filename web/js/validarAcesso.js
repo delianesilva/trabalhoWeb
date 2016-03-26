1a44972c33b567dd25605973e9fcc1b7dbbfb0c4
Maria <Maria@Maria-PC>
2016-03-26 00:40:15 -0400

    web

---
+++ web/js/validarAcesso.js
@@ -0,0 +1,16 @@
+/*function validar(){
+	var login = document.getElementById('nome_id').value;
+	var senha = document.getElementById('senha_id').value;
+	if(login === "" || senha === ""){
+		alert("preencha os campos com usuario e senha respectivamente");
+		return false;
+	}else if(login === "user" && senha === "123"){
+		alert("login autorizado");
+		return true;
+	}else{
+		alert("login negado");
+		return false;
+	}
+}*/
+
+
